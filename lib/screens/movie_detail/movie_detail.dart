01718398cc6910944f7517bd6e91132231ebe25e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-11-20 05:55:38 +0530

    Initial commit

---
+++ lib/screens/movie_detail/movie_detail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MovieDetail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello"),
+      ),
+      body: const Text(''),
+    );
+  }
+}
