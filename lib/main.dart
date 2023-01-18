ba662ca2e8553d859ea40fad80607cf98f1e7c10
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-01-18 12:17:01 +0530

    added movie.dart and movie_fact.dart dynamic loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/movie_detail/movie_detail.dart';
-import 'style.dart';
+import 'app.dart';
 
-main(){
-  runApp(App());
-}
-
-class App extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MovieDetail(),
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          textTheme: TextTheme(titleMedium: AppBarTextStyle),
-        ),
-        textTheme: TextTheme(
-          titleMedium: TitleTextStyle,
-          bodyMedium: Body1TextStyle,
-        )
-      ),
-    );
-  }
-
+void main() {
+  return runApp(App());
 }
---
+++ lib/models/movie_fact.dart
@@ -0,0 +1,6 @@
+class MovieFact{
+  late final String title;
+  late final String text;
+
+  MovieFact(this.title, this.text);
+}
